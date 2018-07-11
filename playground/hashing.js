1e8d324463b8219334e419b8e9d8872f1f2e6f67
Edmond Laja <>
2018-07-11 17:19:09 +0200

    pass hashing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,23 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
+var password = '123abc';
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+/*var data = {
     id: 10
 };
 
 var token = jwt.sign(data, '123abc');
 var decoded = jwt.verify(token, '123abc')
 
-/*var message = 'I am user number 3';
+var message = 'I am user number 3';
 var hash = SHA256(message).toString();
 
 console.log(`Message: ${message}`);
